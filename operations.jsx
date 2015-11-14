dd28c7322351c230f4dc0c08c84f66c549348e80
Dave Pagurek <dave@davepagurek.com>
2015-11-14 16:55:59 -0500

    Add some styles

--- operations.jsx
+++ operations.jsx
@@ -7,12 +7,12 @@ export default class Operation extends React.Component {
   render() { 
   	var valDiv;
 	if(this.props.name == "value") {
-	  valDiv = <div className="value"><input type="text" value={this.props.value} /></div>
+	  valDiv = <div className='value'><input type="text" value={this.props.value} /></div>
 	} else {
 	  valDiv = <h3>{this.props.name}</h3>
 	}
     return (
-      <div className="operations">
+      <div className={`operation ${this.props.type}`}>
       	<div className="type"></div>
       	{valDiv}
       </div>
