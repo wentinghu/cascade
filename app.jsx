bf60bc3fb262d94285894b3fe96cd0ebdc4acc5b
Wenting Hu <wentinghu.1996@gmail.com>
2015-11-14 17:52:23 -0500

    Add menu buttons

--- app.jsx
+++ app.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Operation from './operations.jsx';
 
+var types = {
+  add: "do",
+  sub: "do",
+  reduce: "do"
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -11,7 +17,13 @@ class App extends React.Component {
   }
 
   handleChange(e){
-    this.setState({title: e.target.value})
+    this.setState({title: e.target.value});
+  }
+
+  addToList(valueName){
+    var newArray = this.state.operations.slice();
+    newArray.push({type: types[valueName] || "with", name: valueName});
+    this.setState({operations: newArray});
   }
 
   render() { 
@@ -24,6 +36,12 @@ class App extends React.Component {
         <div className="buttons">
           <button onClick={() => alert("MAKE THIS")}><i className="fa fa-plus"></i></button>
           <button onClick={() => alert("MAKE THIS")} className="run"><i className="fa fa-play"></i></button>
+        </div>
+        <div className="menu">
+          <button onClick={() => this.addToList("add")}>ADD</button>
+          <button onClick={() => this.addToList("sub")}>SUBTRACT</button>
+          <button onClick={() => this.addToList("reduce")}>REDUCE</button>
+          <button onClick={() => this.addToList("value")}>VALUE</button>
         </div>
       </div>
     )
