cef8c4a63c5a11c485f469e4cb251126f6cc7e22
Wenting Hu <wentinghu.1996@gmail.com>
2015-11-14 16:56:30 -0500

    Add input

--- app.jsx
+++ app.jsx
@@ -9,12 +9,17 @@ class App extends React.Component {
       operations: [{type: "with", name: "value", value: 2}, {type: "do", name: "value", value: 2}]
     }
   }
+
+  handleChange(e){
+    this.setState({title: e.target.value})
+  }
+
   render() { 
     return (
       <div className="app">
-        <input className="title" type="text" value={this.state.title} />
+        <input className="title" type="text" value={this.state.title} onChange={this.handleChange}/>
         <div className="operations">
-          {this.state.operations.map((operation) => <Operation type={operation.type} name={operation.name} value={operation.value} />)}
+          {this.state.operations.map((operation) => <Operation type={operation.type} name={operation.name}/>)}
         </div>
         <button onClick={() => alert("MAKE THIS")}><i className="fa fa-plus"></i></button>
         <button onClick={() => alert("MAKE THIS")}><i className="fa fa-play"></i></button>
--- operations.jsx
+++ operations.jsx
@@ -3,11 +3,21 @@ import React from 'react';
 export default class Operation extends React.Component {
   constructor(props) {
     super(props);
+    if(props.name == "value"){
+    	this.state = {
+    		value: 1
+    	}
+    }
   }
+
+  valueChanged(e){
+    this.setState({value: e.target.value})
+  }
+
   render() { 
   	var valDiv;
 	if(this.props.name == "value") {
-	  valDiv = <div className="value"><input type="text" value={this.props.value} /></div>
+	  valDiv = <div className="value"><input type="text" value={this.state.value} onChange={this.valueChanged}/></div>
 	} else {
 	  valDiv = <h3>{this.props.name}</h3>
 	}
