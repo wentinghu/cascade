7c53aa959ad0a2bb520d816b900f56e23532b30e
Dave Pagurek <dave@davepagurek.com>
2015-11-14 15:29:34 -0500

    Add buttons

--- app.jsx
+++ app.jsx
@@ -3,12 +3,20 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {title: "New Procedure"}
+    this.state = {
+      title: "New Procedure",
+      operations: []
+    }
   }
   render() { 
     return (
       <div>
         <input type="text" value={this.state.title} />
+        <div className="operations">
+          {this.state.operations.map((operation) => <Operation settings={operation} />)}
+        </div>
+        <button onClick={() => alert("MAKE THIS")}>+</button>
+        <button onClick={() => alert("MAKE THIS")}>+</button>
       </div>
     )
   }
