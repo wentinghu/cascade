4cb17d50cd6d17df8f30e7a51bc0ab87563c2375
Dave Pagurek <dave@davepagurek.com>
2015-11-15 01:13:25 -0500

    EVALUATING STUFF WORKS WOO

--- operations.jsx
+++ operations.jsx
@@ -3,10 +3,10 @@ import React from 'react/addons';
 export default class Operation extends React.Component {
   constructor(props) {
     super(props);
-    if(props.name == "value"){
-    	this.state = {
-    		value: 1
-    	}
+    if(props.operation.name == "value"){
+      this.state = {
+        value: 1
+      }
     }
   }
 
@@ -15,16 +15,16 @@ export default class Operation extends React.Component {
   }
 
   render() { 
-  	var valDiv;
-  	if(this.props.name == "value") {
-  	  valDiv = <input type="text" value={this.state.value} onChange={this.valueChanged}/>
-  	} else {
-  	  valDiv = <h3>{this.props.name}</h3>
-  	}
+    var valDiv;
+    if(this.props.operation.name == "value") {
+      valDiv = <input type="text" value={this.state.value} onChange={this.valueChanged}/>
+    } else {
+      valDiv = <h3>{this.props.operation.name}</h3>
+    }
     return (
       <div className="operationwrapper">
         <div className="operation">
-          <div className={`type ${this.props.type}`}>{this.props.type}</div>
+          <div className={`type ${this.props.operation.type}`}>{this.props.operation.type}</div>
           <div className="value">{valDiv}</div>
           <button onClick={this.props.remove}>REMOVE</button>
         </div>
